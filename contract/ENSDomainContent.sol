63fa036b2ffd1187e75edead3554eed8801f7d3e
tp-web3 <tpnobug@proton.me>
2022-10-21 14:41:21 +0800

    add ENS Domain check

--- contract/ENSDomainContent.sol
+++ contract/ENSDomainContent.sol
@@ -2,46 +2,45 @@ pragma solidity ^0.8.0;
 
 contract WNSDomainContent {
 
-	mapping(bytes1 => bool) public base_allow;
+    mapping(bytes1 => bool) public base_allow;
     
+    constructor(string memory _strallow) {
+        bytes memory b_strallow = bytes(_strallow);
 
-	constructor(string memory _strallow) {
-		bytes memory b_strallow = bytes(_strallow);
+        for (uint i = 0; i < b_strallow.length; i++) {
+            base_allow[b_strallow[i]] = true;
+        }
 
-		for (uint i = 0; i < b_strallow.length; i++) {
-			base_allow[b_strallow[i]] = true;
-		}
+    }
 
-	}
+    function check_domain(string memory name) public view returns (bool) {
+        bytes memory name_hash = bytes(name);
 
-	function check_domain(string memory name) public view returns (bool) {
-		bytes memory name_hash = bytes(name);
+        for (uint i = 0; i < name_hash.length; i++) {
+            // str_allow[b_strallow[i]] = true;
+            if (!base_allow[name_hash[i]]) {
+                return false;
+            }
+        }
 
-		for (uint i = 0; i < name_hash.length; i++) {
-			// str_allow[b_strallow[i]] = true;
-			if (!base_allow[name_hash[i]]) {
-				return false;
-			}
-		}
+        return true;
+    }
 
-		return true;
-	}
+    function toLower(string memory str) public pure returns (string memory) {
+        bytes memory bStr = bytes(str);
+        bytes memory bLower = new bytes(bStr.length);
 
-    function toLower(string memory str) public view returns (string memory) {
-		bytes memory bStr = bytes(str);
-		bytes memory bLower = new bytes(bStr.length);
+        for (uint i = 0; i < bStr.length; i++) {
+            if ((uint8(bStr[i]) >= 65) && (uint8(bStr[i]) <= 90)) {
 
-		for (uint i = 0; i < bStr.length; i++) {
-			if ((uint8(bStr[i]) >= 65) && (uint8(bStr[i]) <= 90)) {
+                bLower[i] = bytes1(uint8(bStr[i]) + 32);
 
-				bLower[i] = bytes1(uint8(bStr[i]) + 32);
-
-			} else {
-				bLower[i] = bStr[i];
-			}
-		}
+            } else {
+                bLower[i] = bStr[i];
+            }
+        }
         
-		return string(bLower);
-	}
+        return string(bLower);
+    }
 
 }
