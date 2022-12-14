6b51056d9aaf076ed23d70df420aae1c195a78e8
tp-web3 <tpnobug@proton.me>
2022-12-14 15:47:52 +0800

    v0.1

---
+++ contracts/ethregistrar/BaseRegistrarImplementation.sol
@@ -0,0 +1,157 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import "../registry/ENS.sol";
+import "./IBaseRegistrar.sol";
+//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BaseRegistrarImplementation is IBaseRegistrar, Ownable {
+    // A map of expiry times
+    mapping(uint256 => uint256) expiries;
+    // The ENS registry
+    ENS public ens;
+    // The namehash of the TLD this registrar owns (eg, .eth)
+    bytes32 public baseNode;
+    // A map of addresses that are authorised to register and renew names.
+    mapping(address => bool) public controllers;
+    uint256 public constant GRACE_PERIOD = 90 days;
+    bytes4 private constant INTERFACE_META_ID =
+        bytes4(keccak256("supportsInterface(bytes4)"));
+
+    bytes4 private constant RECLAIM_ID =
+        bytes4(keccak256("reclaim(uint256,address)"));
+
+
+
+    constructor(ENS _ens, bytes32 _baseNode) {
+        ens = _ens;
+        baseNode = _baseNode;
+    }
+
+    modifier live() {
+        require(ens.owner(baseNode) == address(this));
+        _;
+    }
+
+    modifier onlyController() {
+        require(controllers[msg.sender]);
+        _;
+    }
+
+
+
+    function addController(address controller) external override onlyOwner {
+        controllers[controller] = true;
+        emit ControllerAdded(controller);
+    }
+
+    // Revoke controller permission for an address.
+    function removeController(address controller) external override onlyOwner {
+        controllers[controller] = false;
+        emit ControllerRemoved(controller);
+    }
+
+    // Set the resolver for the TLD this registrar manages.
+    function setResolver(address resolver) external override onlyOwner {
+        ens.setResolver(baseNode, resolver);
+    }
+
+    // Returns the expiration timestamp of the specified id.
+    function nameExpires(uint256 id) external view override returns (uint256) {
+        return expiries[id];
+    }
+
+    // Returns true iff the specified name is available for registration.
+    function available(uint256 id) public view override returns (bool) {
+        // Not available if it's registered here or in its grace period.
+        return expiries[id] + GRACE_PERIOD < block.timestamp;
+    }
+
+    /**
+     * @dev Register a name.
+     * @param id The token ID (keccak256 of the label).
+     * @param owner The address that should own the registration.
+     * @param duration Duration in seconds for the registration.
+     */
+    function register(
+        uint256 id,
+        address owner,
+        uint256 duration
+    ) external override returns (uint256) {
+        return _register(id, owner, duration, true);
+    }
+
+    /**
+     * @dev Register a name, without modifying the registry.
+     * @param id The token ID (keccak256 of the label).
+     * @param owner The address that should own the registration.
+     * @param duration Duration in seconds for the registration.
+     */
+    function registerOnly(
+        uint256 id,
+        address owner,
+        uint256 duration
+    ) external onlyController returns (uint256) {
+        return _register(id, owner, duration, false);
+    }
+
+    function _register(
+        uint256 id,
+        address owner,
+        uint256 duration,
+        bool updateRegistry
+    ) internal onlyController returns (uint256) {
+        require(available(id));
+        require(
+            block.timestamp + duration + GRACE_PERIOD >
+                block.timestamp + GRACE_PERIOD
+        ); // Prevent future overflow
+
+        expiries[id] = block.timestamp + duration;
+
+
+        if (updateRegistry) {
+            ens.setSubnodeOwner(baseNode, bytes32(id), owner);
+        }
+
+        emit NameRegistered(id, owner, block.timestamp + duration);
+
+        return block.timestamp + duration;
+    }
+
+    function renew(uint256 id, uint256 duration)
+        external
+        override
+        live
+        onlyController
+        returns (uint256)
+    {
+        require(expiries[id] + GRACE_PERIOD >= block.timestamp); // Name must be registered here or in grace period
+        require(
+            expiries[id] + duration + GRACE_PERIOD > duration + GRACE_PERIOD
+        ); // Prevent future overflow
+
+        expiries[id] += duration;
+        emit NameRenewed(id, expiries[id]);
+        return expiries[id];
+    }
+
+    /**
+     * @dev Reclaim ownership of a name in ENS, if you own it in the registrar.
+     */
+    function reclaim(uint256 id, address owner) external onlyController override live {
+        // require(_isApprovedOrOwner(msg.sender, id));
+        ens.setSubnodeOwner(baseNode, bytes32(id), owner);
+    }
+
+    function supportsInterface(bytes4 interfaceID)
+        public
+        view
+        returns (bool)
+    {
+        return
+            interfaceID == INTERFACE_META_ID ||
+            interfaceID == RECLAIM_ID;
+    }
+}
